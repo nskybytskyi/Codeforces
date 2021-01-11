aac58e07145c20c07852886a44d36ff271f6e3f4
Sky-Nik <n.skybytskyi@gmail.com>
2021-01-11 20:59:52 +0200

    rm wrong lca dir

---
+++ template.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+template <typename T = int>
+std::vector<T> ReadArray(int size = 0, std::istream& in_stream = std::cin);
+
+template <typename T>
+void PrintArray(const std::vector<T>& array, std::ostream& out_stream = std::cout,
+                bool size = false, char element_separator = ' ', char end = '\n');
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  // todo
+
+  return 0;
+}
+
+template <typename T>
+std::vector<T> ReadArray(int size, std::istream& in_stream) {
+  if (!size) {
+    in_stream >> size;
+  }
+  std::vector<T> array(size);
+  for (auto& element : array) {
+    in_stream >> element;
+  }
+  return array;
+}
+
+template <typename T>
+void PrintArray(const std::vector<T>& array, std::ostream& out_stream,
+                bool size, char element_separator, char end) {
+  if (size) {
+    out_stream << array.size() << end;
+  }
+  for (const auto& element : array) {
+    out_stream << element << element_separator;
+  }
+  out_stream << end;
+}
