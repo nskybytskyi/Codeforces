21378a6a685f1f9f15a9722a7260de2e6005dc21
Sky-Nik <n.skybytskyi@gmail.com>
2021-01-12 02:13:57 +0200

    lib and template dsu

--- template.cpp
+++ template.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <numeric>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -12,6 +13,17 @@ std::vector<T> ReadArray(int size = 0, std::istream& in_stream = std::cin);
 template <typename T>
 void PrintArray(const std::vector<T>& array, std::ostream& out_stream = std::cout,
                 bool size = false, char element_separator = ' ', char end = '\n');
+
+class DSU {
+  public:
+    explicit DSU(int size);
+    int Find(int vertex) const;
+    void Union(int first, int second);
+
+  private:
+    mutable std::vector<int> parent_;
+    std::vector<int> rank_;
+};
 
 int main() {
   std::ios::sync_with_stdio(false);
@@ -45,4 +57,28 @@ void PrintArray(const std::vector<T>& array, std::ostream& out_stream,
     out_stream << element << element_separator;
   }
   out_stream << end;
+}
+
+DSU::DSU(int size) {
+  rank_.resize(size);
+  parent_.resize(size);
+  std::iota(parent_.begin(), parent_.end(), 0);
+}
+
+int DSU::Find(int vertex) const {
+  return (vertex == parent_[vertex])
+    ? vertex : (parent_[vertex] = Find(parent_[vertex]));
+}
+
+void DSU::Union(int first, int second) {
+  first = Find(first), second = Find(second);
+  if (first != second) {
+    if (rank_[first] < rank_[second]) {
+      std::swap(first, second);
+    }
+    parent_[second] = first;
+    if (rank_[first] == rank_[second]){
+      ++rank_[first];
+    }
+  }
 }
