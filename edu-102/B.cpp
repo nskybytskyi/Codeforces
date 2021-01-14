aa26dc57a96efeb5e7668e38bc0b8a533c233358
Sky-Nik <n.skybytskyi@gmail.com>
2021-01-14 23:58:47 +0200

    upsolced f

--- edu-102/B.cpp
+++ edu-102/B.cpp
@@ -9,22 +9,22 @@ int main() {
   int query_count;
   std::cin >> query_count;
   while (query_count --> 0) {
-    bool lca_exists = true;
-    std::string first, second, lca;
+    bool lcm_exists = true;
+    std::string first, second, lcm;
     std::cin >> first >> second;
 
     const int first_length = first.length(), second_length = second.length();
     int first_index = 0, second_index = 0;
 
     do {
-      lca_exists &= first[first_index] == second[second_index];
-      lca += first[first_index];
+      lcm_exists &= first[first_index] == second[second_index];
+      lcm += first[first_index];
       first_index = (first_index + 1) % first_length;
       second_index = (second_index + 1) % second_length;
-    } while (lca_exists && (first_index || second_index));
+    } while (lcm_exists && (first_index || second_index));
 
-    if (lca_exists) {
-      std::cout << lca << "\n";
+    if (lcm_exists) {
+      std::cout << lcm << "\n";
     } else {
       std::cout << -1 << "\n";
     }
